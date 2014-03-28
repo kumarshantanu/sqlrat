7622f63dfb8bc8de02db56711d6f370834264443
Shantanu Kumar <kumar.shantanu@gmail.com>
2014-03-28 20:45:03 +0530

    upgrade version to cljx 0.3.2, clojurescript.test 0.3.0, Clojure 1.6.0; clean up old CLJS versions and add CLJS 0.0.2197 to test profiles

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/kumarshantanu/sqlrat"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[com.keminglabs/cljx "0.3.1"]
+  :plugins [[com.keminglabs/cljx "0.3.2"]
             [lein-clr     "0.2.1"]
             [lein-cascade "0.1.2"]]
   :clr {:cmd-templates {:clj-url "http://sourceforge.net/projects/clojureclr/files/clojure-clr-1.4.1-Debug-4.0.zip/download"
@@ -23,21 +23,7 @@
                   {:source-paths ["test"] :output-path "target/generated/test-cljs" :rules :cljs}]}
   :source-paths ["src"  "target/generated/clj" "resources"]
   :test-paths   ["test" "target/generated/test-clj"]
-  :profiles {:1847 {:dependencies [[org.clojure/clojurescript "0.0-1847"]]}
-             :1853 {:dependencies [[org.clojure/clojurescript "0.0-1853"]]}
-             :1859 {:dependencies [[org.clojure/clojurescript "0.0-1859"]]}
-             :1877 {:dependencies [[org.clojure/clojurescript "0.0-1877"]]}
-             :1885 {:dependencies [[org.clojure/clojurescript "0.0-1885"]]}
-             :1889 {:dependencies [[org.clojure/clojurescript "0.0-1889"]]}
-             :1909 {:dependencies [[org.clojure/clojurescript "0.0-1909"]]}
-             :1913 {:dependencies [[org.clojure/clojurescript "0.0-1913"]]}
-             :1933 {:dependencies [[org.clojure/clojurescript "0.0-1933"]]}
-             :1934 {:dependencies [[org.clojure/clojurescript "0.0-1934"]]}
-             :1978 {:dependencies [[org.clojure/clojurescript "0.0-1978"]]}
-             :2014 {:dependencies [[org.clojure/clojurescript "0.0-2014"]]}
-             :2024 {:dependencies [[org.clojure/clojurescript "0.0-2024"]]}
-             :2030 {:dependencies [[org.clojure/clojurescript "0.0-2030"]]}
-             :2060 {:dependencies [[org.clojure/clojurescript "0.0-2060"]]}
+  :profiles {:2060 {:dependencies [[org.clojure/clojurescript "0.0-2060"]]}
              :2067 {:dependencies [[org.clojure/clojurescript "0.0-2067"]]}
              :2075 {:dependencies [[org.clojure/clojurescript "0.0-2075"]]}
              :2080 {:dependencies [[org.clojure/clojurescript "0.0-2080"]]}
@@ -47,9 +33,10 @@
              :2138 {:dependencies [[org.clojure/clojurescript "0.0-2138"]]}
              :2156 {:dependencies [[org.clojure/clojurescript "0.0-2156"]]}
              :2173 {:dependencies [[org.clojure/clojurescript "0.0-2173"]]}
+             :2197 {:dependencies [[org.clojure/clojurescript "0.0-2197"]]}
              :cb1 {:plugins [[lein-cljsbuild "1.0.0-alpha1"]]}
              :cb2 {:plugins [[lein-cljsbuild "1.0.2"]]}
-             :jst {:plugins [[com.cemerick/clojurescript.test "0.2.2"]]
+             :jst {:plugins [[com.cemerick/clojurescript.test "0.3.0"]]
                    :hooks [leiningen.cljsbuild]
                    :cljsbuild {:builds [{:source-paths ["target/generated/cljs" "target/generated/test-cljs"]
                                          :compiler {:output-to "target/cljs/testable.js"
@@ -60,7 +47,7 @@
                                                "tests-nodejs"  ["node"      :node-runner "target/cljs/testable.js"]}}}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-alpha3"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :jvm {:test-paths ["test-clj"]}
              ;; Tagged literal tests fail on CLR, so commented out
              :clr-1.4 {:clr {:cmd-templates {:clj-exe [[?PATH "mono"] [CLJCLR14_40 %1]]}}
@@ -71,20 +58,6 @@
   :cascade {:clean [["clean"]]
             :cljx  [["cljx" "once"]]
             :ccljx [:clean :cljx]
-            :1847 [["with-profile" "1847,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :1853 [["with-profile" "1853,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :1859 [["with-profile" "1859,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :1877 [["with-profile" "1877,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :1885 [["with-profile" "1885,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :1889 [["with-profile" "1889,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :1909 [["with-profile" "1909,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :1913 [["with-profile" "1913,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :1933 [["with-profile" "1933,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :1934 [["with-profile" "1934,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :1978 [["with-profile" "1978,jst,cb1" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :2014 [["with-profile" "2014,jst,cb2" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :2024 [["with-profile" "2024,jst,cb2" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :2030 [["with-profile" "2030,jst,cb2" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
             :2060 [["with-profile" "2060,jst,cb2" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
             :2067 [["with-profile" "2067,jst,cb2" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
             :2075 [["with-profile" "2075,jst,cb2" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
@@ -95,9 +68,8 @@
             :2138 [["with-profile" "2138,jst,cb2" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
             :2156 [["with-profile" "2156,jst,cb2" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
             :2173 [["with-profile" "2173,jst,cb2" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
-            :cljs-all [:1847 :1853 :1859 :1877 :1885 :1889 :1909 :1913 :1933 :1934
-                       :1978 :2014 #_:2024 #_:2030  ; tests fail in 2024, 2030 due to array-map
-                       :2060 :2067 :2075 :2080 :2120 :2127 :2134 :2138 :2156 :2173]
+            :2197 [["with-profile" "2197,jst,cb2" "do" "cljsbuild" "clean," "cljsbuild" "test"]]
+            :cljs-all [:2060 :2067 :2075 :2080 :2120 :2127 :2134 :2138 :2156 :2173 :2197]
             :1.4 [["with-profile" "1.4,jvm" "test"]]
             :1.5 [["with-profile" "1.5,jvm" "test"]]
             :1.6 [["with-profile" "1.6,jvm" "test"]]
@@ -106,7 +78,8 @@
             :clr-1.5 [:ccljx ["with-profile" "clr-1.5" "clr" "test"]]
             :pkg [["with-profile" "pkg" %1]]
             "test1.4" [:ccljx :1.4]
-            "testdev" [:ccljx :1.5 :2173]
+            "test1.5" [:ccljx :1.5]
+            "testdev" [:ccljx :1.6 :2197]
             "testjvm" [:ccljx :clj-all]
             "testclr" [:clr-1.4 :clr-1.5]
             "testjs"  [:ccljx :cljs-all]
